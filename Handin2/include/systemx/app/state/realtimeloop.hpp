ee394c4ad312b833e8a3cf846821e36925e4cc14
Kalle Møller <github.com@k-moeller.dk>
2017-10-15 15:49:00 +0200

    And state

---
+++ Handin2/include/systemx/app/state/realtimeloop.hpp
@@ -0,0 +1,51 @@
+#ifndef REALTIMELOOP_HPP_
+#define REALTIMELOOP_HPP_
+
+#include "systemx/app/statemachine.hpp"
+#include "systemx/os/ostream.hpp"
+
+namespace systemx {
+	namespace app {
+		namespace state {
+			namespace base {
+				struct AndStates : IStateData {
+					AndStates(IStateMachine* statemachine1, IStateMachine* statemachine2) : statemachine1(statemachine1), statemachine2(statemachine2) {}
+					IStateMachine* statemachine1;
+					IStateMachine* statemachine2;
+				};
+
+				class RealTimeLoopBase : public IState {
+				public:
+
+					void EnterState(ISystem* system, IStateMachine* statemachine);
+
+					void ExitState(ISystem* system, IStateMachine* statemachine);
+
+					void Exit(ISystem* system, IStateMachine* statemachine);
+
+					void Start(ISystem* system, IStateMachine* statemachine);
+					void Restart(ISystem* system, IStateMachine* statemachine);
+					void Stop(ISystem* system, IStateMachine* statemachine);
+					void Suspend(ISystem* system, IStateMachine* statemachine);
+					void Resume(ISystem* system, IStateMachine* statemachine);
+
+					void SelfTestFailed(ISystem* system, IStateMachine* statemachine);
+					void SelftestOk(ISystem* system, IStateMachine* statemachine);
+					void Initalized(ISystem* system, IStateMachine* statemachine);
+
+					void ConfigX(ISystem* system, IStateMachine* statemachine);
+					void Configure(ISystem* system, IStateMachine* statemachine);
+					void ConfigurationEnded(ISystem* system, IStateMachine* statemachine);
+
+				private:
+					AndStates* substatemachines(IStateMachine* statemachine);
+
+				};
+			}
+
+			using RealTimeLoop = Singleton<base::RealTimeLoopBase>;
+		}
+	}
+}
+
+#endif // !REALTIMELOOP_HPP_
---
+++ Handin2/src/app/state/realtimeloop.cpp
@@ -0,0 +1,89 @@
+#include "systemx/os/iostream.hpp"
+#include "systemx/app/state/initializing.hpp"
+#include "systemx/app/state/realtimeloop.hpp"
+#include "systemx/app/state/poweronselftest.hpp"
+#include "systemx/app/state/ready.hpp"
+#include "systemx/statemachine/statemachine.hpp"
+
+namespace systemx {
+	namespace app {
+		namespace state {
+			namespace base {
+
+				
+
+				void RealTimeLoopBase::EnterState(ISystem* system, IStateMachine* statemachine) {
+					system->logger() << "EnterState: RealTimeLoop" << os::endl;
+					statemachine->state_data = new AndStates{
+						new StateMachine(system, Ready::Instance()),
+						new StateMachine(system, Ready::Instance())
+					};
+				}
+
+				void RealTimeLoopBase::ExitState(ISystem* system, IStateMachine* statemachine) {
+					delete substatemachines(statemachine)->statemachine1;
+					delete substatemachines(statemachine)->statemachine2;
+					delete statemachine->state_data;
+					system->logger() << "ExitState: RealTimeLoop" << os::endl;
+				}
+
+				AndStates* RealTimeLoopBase::substatemachines(IStateMachine* statemachine) {
+					return static_cast<AndStates*>(statemachine->state_data);
+				}
+
+				void RealTimeLoopBase::Restart(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->Restart();
+					substatemachines(statemachine)->statemachine2->Restart();
+				}
+
+				void RealTimeLoopBase::Exit(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->Exit();
+					substatemachines(statemachine)->statemachine2->Exit();
+				};
+
+				void RealTimeLoopBase::Start(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->Start();
+					substatemachines(statemachine)->statemachine2->Start();
+				};
+				void RealTimeLoopBase::Stop(ISystem* system, IStateMachine* statemachine) {
+					statemachine->ChangeState(Ready::Instance());
+				};
+				void RealTimeLoopBase::Suspend(ISystem* system, IStateMachine* statemachine) {
+					//statemachine->ChangeState(Suspend::Instance());
+				};
+				void RealTimeLoopBase::Resume(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->Resume();
+					substatemachines(statemachine)->statemachine2->Resume();
+				};
+
+				void RealTimeLoopBase::SelfTestFailed(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->SelfTestFailed();
+					substatemachines(statemachine)->statemachine2->SelfTestFailed();
+				};
+				void RealTimeLoopBase::SelftestOk(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->SelftestOk();
+					substatemachines(statemachine)->statemachine2->SelftestOk();
+				};
+				void RealTimeLoopBase::Initalized(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->Initalized();
+					substatemachines(statemachine)->statemachine2->Initalized();
+				};
+
+				void RealTimeLoopBase::ConfigX(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->ConfigX();
+					substatemachines(statemachine)->statemachine2->ConfigX();
+				};
+				void RealTimeLoopBase::Configure(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->Configure();
+					substatemachines(statemachine)->statemachine2->Configure();
+				};
+				void RealTimeLoopBase::ConfigurationEnded(ISystem* system, IStateMachine* statemachine) {
+					substatemachines(statemachine)->statemachine1->ConfigurationEnded();
+					substatemachines(statemachine)->statemachine2->ConfigurationEnded();
+				};
+
+			}
+		}
+	}
+}
+
