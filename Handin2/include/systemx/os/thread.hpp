2b590e893c71cb037b75b87b2e463eef2eb0e737
KasperCK <kapperck@gmail.com>
2017-10-15 17:33:26 +0200

    base thread

--- Handin2/include/systemx/os/thread.hpp
+++ Handin2/include/systemx/os/thread.hpp
@@ -1,11 +1,22 @@
 #ifndef THREAD_HPP_
 #define THREAD_HPP_
 
+#include <thread>
 
 namespace systemx {
     namespace os {
         class thread{
+        public:
+            virtual void start(){
+                _thread = std::thread(&thread::run, this)
+            }
+            ~thread(){
+                thread_.join();
+            }
+        private:
+            virtual void run() = 0;
 
+            std::thread _thread;
         };
     }
 }
