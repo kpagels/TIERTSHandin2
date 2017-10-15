efea94f53c958915b607d59c1734f75fb307fbfd
KasperCK <kapperck@gmail.com>
2017-10-15 17:41:49 +0200

    protected

--- Handin2/include/systemx/os/thread.hpp
+++ Handin2/include/systemx/os/thread.hpp
@@ -12,14 +12,14 @@ namespace systemx {
                 _thread = std::thread(&thread::run, this)
             }
             ~thread(){
-                std::lock_guard<std::mutex> guard(_mutex);
                 thread_.join();
             }
-        private:
+        protected:
             virtual void run() = 0;
-
-            std::thread _thread;
+            
             std::mutex _mutex;
+            std::thread _thread;
+            
         };
     }
 }
