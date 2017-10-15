1da7a04f2b0828bfdccc40e9b18a87de3326457e
KasperCK <kapperck@gmail.com>
2017-10-15 17:59:01 +0200

    scoped_lock

---
+++ Handin2/include/systemx/os/scoped_lock.hpp
@@ -0,0 +1,14 @@
+#ifndef SCOPED_LOCK_HPP_
+#define SCOPED_LOCK_HPP_
+
+#include <mutex>
+
+namespace systemx {
+    namespace os {
+    
+    using scoped_lock = std::scoped_lock;
+
+    }
+}
+
+#endif // !SCOPED_LOCK_HPP_
